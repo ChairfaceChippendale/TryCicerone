0c137ce73f8c1b76556f75d686002577597eec62
olexander <ujujzk@gmail.com>
2018-11-30 18:27:02 +0200

    fragment manager

--- app/src/main/java/com/ujujzk/trycicerone/MainActivity.kt
+++ app/src/main/java/com/ujujzk/trycicerone/MainActivity.kt
@@ -2,11 +2,28 @@ package com.ujujzk.trycicerone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun selectTab(name: String){
+
+        val currentFragment = supportFragmentManager.fragments.find { it.isVisible}
+        val newFragment: Fragment? = supportFragmentManager.findFragmentByTag(name)
+
+        if (currentFragment == newFragment) return
+
+        supportFragmentManager.beginTransaction().apply {
+            if (newFragment == null) add(R.id.container, , name)
+            if (currentFragment != null) hide(currentFragment)
+            if (newFragment != null) show(newFragment)
+            commitNow()
+        }
+
     }
 }
